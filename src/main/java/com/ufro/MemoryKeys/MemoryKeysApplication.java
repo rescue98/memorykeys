3d90c82f820ea2fa44ea5e0cab3f1a759e87879b
Diego-Hidra <38220071+Diego-Hidra@users.noreply.github.com>
2022-05-03 15:36:21 -0400

    InicializacionSpring
    Se crea el proyecto por medio de SpringInitializr

---
+++ src/main/java/com/ufro/MemoryKeys/MemoryKeysApplication.java
@@ -0,0 +1,13 @@
+package com.ufro.MemoryKeys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoryKeysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemoryKeysApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ufro/MemoryKeys/MemoryKeysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ufro.MemoryKeys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemoryKeysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
